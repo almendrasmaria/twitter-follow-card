ac487871274efc44c3e2e6764130eab505b82892
NAHUEL GARCIA <nahuel.garcia@rcpo.com.ar>
2025-06-09 20:54:40 -0300

    harcode array de datos de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import TwitterFollowCard from './TwitterFollowCard'
-import './App.css'
+import TwitterFollowCard from "./TwitterFollowCard";
+import "./App.css";
+
+const users = [
+  {
+    userName: "ngarcia",
+    name: "Nahuel Garcia",
+    isFollowing: true,
+  },
+  {
+    userName: "malmendras",
+    name: "María Almendras",
+    isFollowing: false,
+  },
+  {
+    userName: "tecmac",
+    name: "Cristhian Maco",
+    isFollowing: false,
+  },
+  {
+    userName: "jluque",
+    name: "Josue Luque",
+    isFollowing: false,
+  },
+];
 
 function App() {
   return (
     <>
       <TwitterFollowCard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
