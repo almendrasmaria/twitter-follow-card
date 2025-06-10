1b055599e77683f3e2125de815231dad666e53e9
maria <almendrasmaria04@gmail.com>
2025-06-09 22:27:36 -0300

    estilizando buttons

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -3,14 +3,21 @@ import { useState } from "react";
 export default function TwitterFollowCard() {
     const [IsFollowing, setIsFollowing] = useState(false);
 
-    const Button = IsFollowing ? "Siguiendo" : "Seguir"
+    const button = IsFollowing ? "Siguiendo" : "Seguir"
+
+    const colorState = IsFollowing ? 'button-following-card' : 'button-follow-card'; 
+
 
     function handleClick() {
         setIsFollowing(!IsFollowing)
     }
 
     return (
-        <button onClick={handleClick}>{Button}</button>
+        <button onClick={handleClick} className={colorState}>
+            {button} 
+            <span className="button-unfollow-card">dejar de seguir</span>
+        </button>
+        
     )
 
 }
